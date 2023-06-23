0a421d7d04a9dfda41f2bb529456895f562eae10
LukeBresler <guitarmanluke82@gmail.com>
2023-06-23 10:34:59 +0200

    Initial Commit

---
+++ Varys's Book of Little Birds/Varys_s_Book_of_Little_BirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Varys_s_Book_of_Little_BirdsApp.swift
+//  Varys's Book of Little Birds
+//
+//  Created by Luke Bresler on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Varys_s_Book_of_Little_BirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
